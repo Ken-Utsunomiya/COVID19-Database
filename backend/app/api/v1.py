84f5b1dd746dcf0507dde972a79ec1f6074a217c
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-12-09 13:57:16 -0800

    feat: add doctor router

--- backend/app/api/v1.py
+++ backend/app/api/v1.py
@@ -1,7 +1,9 @@
 from fastapi import APIRouter
 
 from app.api.endpoints import hospital
+from backend.app.api.endpoints import doctor
 
 api_router = APIRouter()
 
 api_router.include_router(hospital.router, prefix="/hospitals", tags=["hospitals"])
+api_router.include_router(doctor.router, prefix="/doctors", tags=["doctors"])
