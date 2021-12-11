130445876f91cc4706d93bdd63d251730a200f7b
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-12-10 19:42:16 -0800

    feat: add nurse router

--- backend/app/api/v1.py
+++ backend/app/api/v1.py
@@ -1,9 +1,10 @@
 from fastapi import APIRouter
 
 from app.api.endpoints import hospital
-from backend.app.api.endpoints import doctor
+from backend.app.api.endpoints import doctor, nurse
 
 api_router = APIRouter()
 
 api_router.include_router(hospital.router, prefix="/hospitals", tags=["hospitals"])
 api_router.include_router(doctor.router, prefix="/doctors", tags=["doctors"])
+api_router.include_router(nurse.router, prefix="/nurses", tags=["nurses"])
