4ed0425781133a513e0c126c63e6a16cf5dd6845
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-12-15 11:10:33 -0800

    feat: add health record router

--- backend/app/api/v1.py
+++ backend/app/api/v1.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from app.api.endpoints import hospital
-from backend.app.api.endpoints import doctor, nurse, patient
+from backend.app.api.endpoints import doctor, health_record, nurse, patient
 
 api_router = APIRouter()
 
@@ -9,3 +9,4 @@ api_router.include_router(hospital.router, prefix="/hospitals", tags=["hospitals
 api_router.include_router(doctor.router, prefix="/doctors", tags=["doctors"])
 api_router.include_router(nurse.router, prefix="/nurses", tags=["nurses"])
 api_router.include_router(patient.router, prefix="/patients", tags=["patients"])
+api_router.include_router(health_record.router, prefix="/health_records", tags=["health_records"])
