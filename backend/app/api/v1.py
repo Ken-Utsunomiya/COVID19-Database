e2612427e5102e7db8c92bcfbe7ed3910086522f
Ken-Utsunomiya <kenutsunomiya08@gmail.com>
2021-12-18 16:35:43 -0800

    feat: add bed router

--- backend/app/api/v1.py
+++ backend/app/api/v1.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter
 
 from app.api.endpoints import hospital
-from backend.app.api.endpoints import doctor, guardian, health_record, insurance, nurse, patient
+from backend.app.api.endpoints import bed, doctor, guardian, health_record, insurance, nurse, patient
 
 api_router = APIRouter()
 
@@ -12,3 +12,4 @@ api_router.include_router(patient.router, prefix="/patients", tags=["patients"])
 api_router.include_router(health_record.router, prefix="/health_records", tags=["health_records"])
 api_router.include_router(guardian.router, prefix="/guardians", tags=["guardians"])
 api_router.include_router(insurance.router, prefix="/insurances", tags=["insurances"])
+api_router.include_router(bed.router, prefix="/beds", tags=["beds"])
